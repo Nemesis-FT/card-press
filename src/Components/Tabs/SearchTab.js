ece324584eb74e0f43c3ef3b56f19963482c1145
NemesisFT <lorenzo.balugani@gmail.com>
2024-08-20 15:53:38 +0200

    feat: improved filtering, add sorting

--- src/Components/Tabs/SearchTab.js
+++ src/Components/Tabs/SearchTab.js
@@ -8,6 +8,7 @@ import Table from 'react-bootstrap/Table';
 import Panel from "../Bricks/Panel";
 import Filters from "./Filters";
 import Button from "react-bootstrap/Button";
+import Style from "./SearchTab.module.css"
 
 export default function SearchTab() {
     const {config, storage} = useAppContext()
@@ -18,13 +19,52 @@ export default function SearchTab() {
 
     const [data, setData] = useState([]);
 
+    const levelLookup = {
+        0:"Cantrip",
+        1:"1st Level",
+        2:"2nd Level",
+        3:"3rd Level",
+        4:"4th Level",
+        5:"5th Level",
+        6:"6th Level",
+        7:"7th Level",
+        8:"8th Level",
+        9:"9th Level"
+    }
+
     useEffect(() => {
-        console.debug("Filters:" +filters)
-        if(filters.length===0){
+        console.debug("Filters:" + filters)
+        if (filters.length === 0) {
             return
         }
         retrieveSources().then(r => console.log("Done"));
     }, [filters]);
+
+    async function getClassLookup(sources) {
+        let tmp = await fetch(backend.url + "/data/generated/gendata-spell-source-lookup.json")
+        let json = await tmp.json()
+        let lookups = []
+        for (let source of sources) {
+            if (source.value === "custom") {
+                // Find some way to add the custom bindings...
+                continue
+            }
+            if (json[source.value] === null) {
+                console.error("Warning: undefined source in lookup! Skipping...")
+                continue
+            }
+            for (let key of Object.keys(json[source.value])) {
+                let classList = []
+                for (let sourceClass of Object.keys(json[source.value][key]["class"])) {
+                    for (let cls of Object.keys(json[source.value][key]["class"][sourceClass])) {
+                        classList.push(cls)
+                    }
+                }
+                lookups.push({name: key, value: classList})
+            }
+        }
+        return lookups
+    }
 
     async function retrieveSources() {
         let sources = filters.filter(elem => elem.type === "source")
@@ -49,48 +89,62 @@ export default function SearchTab() {
             let json = await tmp.json()
             data.push(...json.spell)
         }
+        // Provide class data
+
+        let lookups = await getClassLookup(sources)
+
+        data = data.map(spell => {
+            let lookup = lookups.find(lookup => spell.name.toLowerCase() === lookup.name.toLowerCase())
+            return {...spell, classes: lookup.value}
+        })
 
         // Run class filters
         let classes = filters.filter(elem => elem.type === "class").map(elem => elem.value)
-        if(classes.length!==0){
-            let tmp = await fetch(backend.url+"/data/generated/gendata-spell-source-lookup.json")
-            let json = await tmp.json()
-            let lookups = []
-            for(let source of sources){
-                if(source.value === "custom"){
-                    // Find some way to add the custom bindings...
-                    continue
-                }
-                if(json[source.value]===null){
-                    console.error("Warning: undefined source in lookup! Skipping...")
-                    continue
-                }
-                for(let key of Object.keys(json[source.value])){
-                    let classList = []
-                    for(let sourceClass of Object.keys(json[source.value][key]["class"])){
-                        for(let cls of Object.keys(json[source.value][key]["class"][sourceClass])){
-                            classList.push(cls)
-                        }
-                    }
-                    lookups.push({name: key, value: classList})
-                }
-            }
+        if (classes.length !== 0) {
             data = data.filter(spell => {
                 // Find the spell in the lookup table
-                let lookup = lookups.find(lookup => spell.name.toLowerCase()===lookup.name.toLowerCase())
-                // Check if the class is in the class filters
-                if(lookup===null){
-                    return false
-                }
-                for(let cls of lookup.value){
-                    if(classes.includes(cls)){
+                for (let cls of spell.classes) {
+                    if (classes.includes(cls)) {
                         return true
                     }
                 }
                 return false
             })
         }
+        let levels = filters.filter(elem => elem.type === "level").map(elem=>elem.value)
+        if(levels.length !== 0){
+            data = data.filter(spell => levels.includes(levelLookup[spell.level]))
+        }
         setData(data)
+    }
+
+    function arraysEqual(a, b) {
+        if (a === b) return true;
+        if (a == null || b == null) return false;
+        if (a.length !== b.length) return false;
+        for (var i = 0; i < a.length; ++i) {
+            if (a[i] !== b[i]) return false;
+        }
+        return true;
+    }
+
+    function sortBy(field){
+        let tmp = [...data]
+        console.debug("Sorting by "+field)
+        tmp.sort(function(a, b) {
+            let textA = a[field].toString().toUpperCase();
+            let textB = b[field].toString().toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        })
+        // it was already sorted!
+        if(arraysEqual(tmp, data)){
+            tmp.sort(function(a, b) {
+                let textA = a[field].toString().toUpperCase();
+                let textB = b[field].toString().toUpperCase();
+                return (textA < textB) ? 1 : (textA > textB) ? -1 : 0;
+            })
+        }
+        setData(tmp)
     }
 
     if (config.sources.length === 0) {
@@ -102,7 +156,7 @@ export default function SearchTab() {
     return (
         <div>
             <Row>
-                <Col sm={8}>
+                <Col sm={10}>
                     <Form>
                         <Form.Group as={Row} className="mb-3">
                             <Col>
@@ -121,29 +175,31 @@ export default function SearchTab() {
                 <Col sm={2}>
                     <Filters filters={filters} setFilters={setFilters}/>
                 </Col>
-                <Col sm={2}><Button variant={"light"} onClick={retrieveSources}> Search </Button>
-                </Col>
             </Row>
             <Row>
                 <Col sm={6}>
-                    <Table striped bordered hover>
-                        <thead>
-                        <tr>
-                            <th>Spell name</th>
-                            <th>Level</th>
-                            <th>School</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {data.map(spell => (
+                    <div className={Style.ScrollableLeft}>
+                        <Table striped bordered hover>
+                            <thead className={Style.TableHead}>
                             <tr>
-                                <td>{spell.name}</td>
-                                <td>{spell.level}</td>
-                                <td>{spell.school}</td>
+                                <th className={Style.TableHeadElement} onClick={event => {sortBy("name")}}>Spell name</th>
+                                <th className={Style.TableHeadElement} onClick={event => {sortBy("level")}}>Level</th>
+                                <th className={Style.TableHeadElement} onClick={event => {sortBy("school")}}>School</th>
                             </tr>
-                        ))}
-                        </tbody>
-                    </Table>
+                            </thead>
+
+                            <tbody className={Style.Scrollable}>
+                            {data.map(spell => (
+                                <tr>
+                                    <td>{spell.name}</td>
+                                    <td>{spell.level}</td>
+                                    <td>{spell.school}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+
+                        </Table>
+                    </div>
                 </Col>
                 <Col sm={6}>
 
